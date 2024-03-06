367e489b556ae0fada43f1f42139b49f2514fc3a
Alifa Hanania Ardha <alifa.hanania@ui.ac.id>
2024-03-06 19:38:35 +0700

    [REFACTOR] Add new methods

--- src/main/java/id/ac/ui/cs/advprog/eshop/enums/PaymentStatus.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/enums/PaymentStatus.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 
 @Getter
 public enum PaymentStatus {
-    PENDING("PENDING"),
     REJECTED("REJECTED"),
     SUCCESS("SUCCESS");
 
--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -19,27 +19,75 @@ public class Payment {
 
     public Payment(String id, String method, Order order, Map<String, String> paymentData) {
         this.id = id;
-        this.paymentMethod = method;
-        this.status = PaymentStatus.PENDING.getValue();
-        this.order = order;
+        this.setPaymentMethod(method);
+        this.setStatus(status);
 
         if (paymentData.isEmpty()) {
             throw new IllegalArgumentException();
         } else {
             this.paymentData = paymentData;
         }
+
+        if (order == null) {
+            throw new IllegalArgumentException("Order must have at least one product");
+        } else {
+            this.order = order;
+        }
     }
 
-    public Payment(String id, String method, String status, Order order, Map<String, String> paymentData) {
-        this(id, method, order, paymentData);
-        this.setStatus(status);
+    public boolean isValidBankTransfer() {
+        if (this.paymentData.containsKey("bankName") &&
+                this.paymentData.containsKey("referenceCode") ){
+            if (paymentData.get("bankName")!= null){
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isValidVoucherCode(String voucherCode) {
+        if (voucherCode.length() != 16) {
+            return false;
+        }
+        if (!voucherCode.startsWith("ESHOP")) {
+            return false;
+        }
+        String numericalPart = voucherCode.substring(5, 15);
+        if (!numericalPart.matches("\\d{8}")) {
+            return false;
+        }
+        return true;
     }
 
     public void setStatus(String status) {
-        if (PaymentStatus.contains(status)) {
-            this.status = status;
+        if (this.paymentMethod.equals("BANK_TRANSFER")){
+            if (isValidBankTransfer()){
+                this.status = PaymentStatus.SUCCESS.getValue();
+            } else {
+                this.status = PaymentStatus.REJECTED.getValue();
+            }
         } else {
-            throw new IllegalArgumentException();
+            if (paymentData.containsKey("voucherCode")){
+                if (isValidVoucherCode(paymentData.get("voucherCode"))){
+                    this.status = PaymentStatus.SUCCESS.getValue();
+                } else {
+                    this.status = PaymentStatus.REJECTED.getValue();
+                }
+            } else {
+                this.status = PaymentStatus.REJECTED.getValue();
+            }
+
+        }
+    }
+
+    public void setPaymentMethod(String method) {
+        if (PaymentMethod.contains(method)) {
+            this.paymentMethod = method;
+        } else {
+            throw new IllegalArgumentException("Invalid Payment Method");
         }
     }
 }
